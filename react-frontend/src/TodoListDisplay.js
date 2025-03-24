2bccddc844e97be902fa8c8a4b208a4555d602b6
Leopold Blum <leopold.blum@gmx.de>
2025-03-24 20:35:38 +0100

    refactored todolistdisplay, now less boilerplate

--- react-frontend/src/TodoListDisplay.js
+++ react-frontend/src/TodoListDisplay.js
@@ -5,82 +5,43 @@ import TodoEditOrAddButton from "./TodoEditOrAddButton";
 
 const TodoListDisplay = ({ displayFulfilled, todos, activeTodos, toggleDesc, updateList }) => {
 
-    if (displayFulfilled === false) {
+    return (
+        <div className="all-todos-container">
+            {todos != null && todos.filter(entries => entries.fulfilled === displayFulfilled).map((entries) => (
 
-        return (
-            <div className="all-todos-container">
+                <div className={`todoEntry-container ${displayFulfilled ? "todoEntryCompleted-container" : ""}`} key={entries.id}>
 
-                {todos != null && todos.filter(entries => entries.fulfilled === false).map((entries) => (
+                    <div className={`todoEntry-box todo-title ${displayFulfilled ? "todo-title-linethrough" : ""}`} onClick={() => toggleDesc(entries.id)}>
+                        {entries.title}
+                    </div>
 
-                    <div className='todoEntry-container' key={entries.id}>
 
-                        <div className="todoEntry-box todo-title" onClick={() => toggleDesc(entries.id)}> {entries.title} </div>
-
-                        <div className="todoEntry-box">
-                            <div className="todoEntry-box-placeholder-container">
-                                <TodoEditOrAddButton isEdit={true} currentTodo={entries} updateList={updateList} />
-                            </div>
-
-                            <div className="todoEntry-box-placeholder-container">
-                                <TodoDeleteButton currentTodo={entries} updateList={updateList} />
-                            </div>
-
-                            <div className="todoEntry-box-placeholder-container checkbox">
-                                <TodoCustomCheckmark currentTodo={entries} updateList={updateList} checked={displayFulfilled} />
-                            </div>
-
+                    <div className="todoEntry-box">
+                        <div className="todoEntry-box-placeholder-container">
+                            <TodoEditOrAddButton currentTodo={entries} updateList={updateList} />
                         </div>
 
-                        <div className={`todoEntry-desc-popup ${activeTodos.includes(entries.id) ? "visible" : "hidden"}`}>
-                            <div className="todoEntry-desc-text">
-                                <p>{entries.desc}</p>
-                            </div>
+                        <div className="todoEntry-box-placeholder-container">
+                            <TodoDeleteButton currentTodo={entries} updateList={updateList} />
+                        </div>
+
+                        <div className="todoEntry-box-placeholder-container checkbox">
+                            <TodoCustomCheckmark currentTodo={entries} updateList={updateList} checked={displayFulfilled} />
                         </div>
 
                     </div>
-                ))
-                }
-            </div>
-        )
 
-    }
-    else {
-
-        return (
-            <div className="all-todos-container">
-                {todos != null && todos.filter(entries => entries.fulfilled === true).map((entries) => (
-
-                    <div className='todoEntry-container todoEntryCompleted-container' key={entries.id}>
-
-                        <div className="todoEntry-box todo-title todo-title-linethrough " onClick={() => toggleDesc(entries.id)}>  {entries.title}  </div>
-
-
-                        <div className="todoEntry-box">
-                            <div className="todoEntry-box-placeholder-container">
-                                <TodoEditOrAddButton isEdit={true} currentTodo={entries} updateList={updateList} />
-                            </div>
-
-                            <div className="todoEntry-box-placeholder-container">
-                                <TodoDeleteButton currentTodo={entries} updateList={updateList} />
-                            </div>
-
-                            <div className="todoEntry-box-placeholder-container checkbox">
-                                <TodoCustomCheckmark currentTodo={entries} updateList={updateList} checked={displayFulfilled} />
-                            </div>
-
-                        </div>
-
-                        <div className={`todoEntry-desc-popup ${activeTodos.includes(entries.id) ? "visible" : "hidden"}`}>
-                            <div className="todoEntry-desc-text">{entries.desc}</div>
-                        </div>
-
+                    <div className={`todoEntry-desc-popup ${activeTodos.includes(entries.id) ? "visible" : "hidden"}`}>
+                        <div className="todoEntry-desc-text">{entries.desc}</div>
                     </div>
-                ))
-                }
-            </div>
-        )
 
-    }
+                </div>
+            ))
+            }
+        </div>
+    )
+
 }
+
 
 export default TodoListDisplay;
