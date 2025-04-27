e78f580ee15730cc10f63e4c845470ea670c4a35
Leopold Blum <leopold.blum@gmx.de>
2025-04-27 02:59:25 +0200

    refactored endpoints to own file

--- react-frontend/src/api/endpoints.js
+++ react-frontend/src/api/endpoints.js
@@ -1,1 +1,130 @@
-// move helper function here
+/**
+ * =============================
+ * Section: fetching
+ * =============================
+ */
+
+export const fetchAllTodos = async () => {
+    try {
+
+        const response = await fetch("http://localhost:8080/todos")
+        const allEntries = await response.json();
+
+        return allEntries;
+    }
+    catch (error) {
+        console.error("recv error:", error);
+        throw error;
+    }
+}
+
+/**
+ * =============================
+ * Section: CRUD 
+ * =============================
+ */
+
+export async function addTodo(inputTitle, inputDesc) {
+
+    const todoBody = { title: inputTitle, desc: inputDesc, fulfilled: false };
+
+    try {
+        const postNewTodoResponse = await fetch(
+            "http://localhost:8080/todo",
+            {
+                method: "POST",
+                body: JSON.stringify(todoBody),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            },
+        );
+        if (!postNewTodoResponse.ok) {
+            throw new Error(
+                "Error - Response Status:" + postNewTodoResponse.status,
+            );
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+
+export async function editTodo(id, title, desc, fulfilled) {
+
+    const todoBody = { title: title, desc: desc, fulfilled: fulfilled };
+
+    try {
+        const updateResponse = await fetch(
+            "http://localhost:8080/updateTodo/" + id,
+            {
+                method: "POST",
+                body: JSON.stringify(todoBody),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            },
+        );
+        if (!updateResponse.ok) {
+            throw new Error(
+                "Error - Response Status:" + updateResponse.status,
+            );
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+export
+    async function deleteTodo(todoID) {
+    if (todoID === null)
+        throw new Error("todoID is null, cant delete todo!");
+
+    try {
+        const response = await fetch(
+            "http://localhost:8080/todo/" + todoID,
+            {
+                method: "DELETE",
+            },
+        );
+
+        // console.log("deleteResponse: " + response.status);
+
+        if (!response.ok) {
+            throw new Error("Error - Response Status:" + response.status);
+        }
+
+        // todoFuncAndData.updateList();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+/**
+ * =============================
+ * Section: additional 
+ * =============================
+ */
+
+
+export async function deleteAllFulfilledTodos() {
+    try {
+        const response = await fetch(
+            "http://localhost:8080/deleteAllFulfilledTodos",
+            {
+                method: "DELETE",
+            },
+        );
+
+
+        if (!response.ok) {
+            throw new Error("Error - Response Status:" + response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
--- react-frontend/src/api/queriesAndMutations.js
+++ react-frontend/src/api/queriesAndMutations.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-
+import { fetchAllTodos, addTodo, editTodo, deleteTodo, deleteAllFulfilledTodos } from './endpoints';
 
 /**
  * =============================
@@ -7,20 +7,6 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
  * =============================
  */
 
-
-const fetchAllTodos = async () => {
-    try {
-
-        const response = await fetch("http://localhost:8080/todos")
-        const allEntries = await response.json();
-
-        return allEntries;
-    }
-    catch (error) {
-        console.error("recv error:", error);
-        throw error;
-    }
-}
 
 /**
  * @return fetches and returns todos from server 
@@ -76,31 +62,6 @@ export const useMutationAddTodo = () => {
     })
 }
 
-async function addTodo(inputTitle, inputDesc) {
-
-    const todoBody = { title: inputTitle, desc: inputDesc, fulfilled: false };
-
-    try {
-        const postNewTodoResponse = await fetch(
-            "http://localhost:8080/todo",
-            {
-                method: "POST",
-                body: JSON.stringify(todoBody),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            },
-        );
-        if (!postNewTodoResponse.ok) {
-            throw new Error(
-                "Error - Response Status:" + postNewTodoResponse.status,
-            );
-        }
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
 
 export const useMutationEditTodo = (currentTodo) => {
 
@@ -132,35 +93,8 @@ export const useMutationEditTodo = (currentTodo) => {
 
         onSettled: () => queryClient.invalidateQueries({ queryKey: ['todos'] }),
     })
-
 }
 
-async function editTodo(id, title, desc, fulfilled) {
-
-    const todoBody = { title: title, desc: desc, fulfilled: fulfilled };
-
-    try {
-        const updateResponse = await fetch(
-            "http://localhost:8080/updateTodo/" + id,
-            {
-                method: "POST",
-                body: JSON.stringify(todoBody),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            },
-        );
-        if (!updateResponse.ok) {
-            throw new Error(
-                "Error - Response Status:" + updateResponse.status,
-            );
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-
-}
 
 /**
  * 
@@ -195,30 +129,6 @@ export const useMutationDeleteTodo = () => {
 
         onSettled: () => queryClient.invalidateQueries({ queryKey: ['todos'] }),
     })
-}
-
-async function deleteTodo(todoID) {
-    if (todoID === null)
-        throw new Error("todoID is null, cant delete todo!");
-
-    try {
-        const response = await fetch(
-            "http://localhost:8080/todo/" + todoID,
-            {
-                method: "DELETE",
-            },
-        );
-
-        // console.log("deleteResponse: " + response.status);
-
-        if (!response.ok) {
-            throw new Error("Error - Response Status:" + response.status);
-        }
-
-        // todoFuncAndData.updateList();
-    } catch (error) {
-        console.error(error);
-    }
 }
 
 
@@ -257,22 +167,4 @@ export const useMutateDAFT = () => {
 
         onSettled: () => queryClient.invalidateQueries({ queryKey: ['todos'] }),
     })
-}
-
-async function deleteAllFulfilledTodos() {
-    try {
-        const response = await fetch(
-            "http://localhost:8080/deleteAllFulfilledTodos",
-            {
-                method: "DELETE",
-            },
-        );
-
-
-        if (!response.ok) {
-            throw new Error("Error - Response Status:" + response.status);
-        }
-    } catch (error) {
-        console.error(error);
-    }
 }
