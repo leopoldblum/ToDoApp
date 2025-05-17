7871783369fe1f4826c12fa67a17cc5ea55bf648
Leopold Blum <leopold.blum@gmx.de>
2025-05-18 01:40:51 +0200

    added hover color to checkbox without breaking logic

--- react-frontend/src/TodoCustomCheckmark.jsx
+++ react-frontend/src/TodoCustomCheckmark.jsx
@@ -22,37 +22,36 @@ const TodoCustomCheckmark = ({ currentTodo, checked }) => {
         setIsChecked(prev => !prev);
     };
 
-    const handleTransitionEnd = () => {
+    const handleTransitionEnd = (e) => {
 
-        if (!hasTransitionStartedRef.current) {
+        if ((e.propertyName !== "opacity" && e.propertyName !== "transform") || hasTransitionStartedRef.current) return;
 
-            mutationEditTodo.mutate({
-                id: currentTodo.id,
-                title: currentTodo.title,
-                desc: currentTodo.desc,
-                fulfilled: !currentTodo.fulfilled,
-                userid: todoFuncAndData.userIDref.current
-            });
+        mutationEditTodo.mutate({
+            id: currentTodo.id,
+            title: currentTodo.title,
+            desc: currentTodo.desc,
+            fulfilled: !currentTodo.fulfilled,
+            userid: todoFuncAndData.userIDref.current
+        });
 
-            hasTransitionStartedRef.current = true
-        }
+        hasTransitionStartedRef.current = true
+
     };
 
     return (
         <div className="flex justify-center items-center min-w-15 ">
 
 
-            <div className="flex justify-center items-center w-9 h-9 relative font-medium cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out "
+            <div className="flex justify-center items-center w-9 h-9 relative font-medium cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out group"
                 onClick={handleClick}
-            // onTransitionEnd={handleTransitionEnd}
+                onTransitionEnd={handleTransitionEnd}
             >
 
-                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out  ${isChecked ? "opacity-0 scale-50 rotate-20" : "opacity-100 scale-100"}`}
-                    onTransitionEnd={handleTransitionEnd}>
+                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out group-hover:text-text-hover-lm  ${!isChecked ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-75 rotate-45"}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2" /></svg>
                 </div>
 
-                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out ${!isChecked ? "opacity-0 scale-50 rotate-20 " : "opacity-100 scale-100"}`}>
+                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out group-hover:text-text-hover-lm ${isChecked ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-75 rotate-45"}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2" /><path d="m9 12 2 2 4-4" /></svg>
                 </div>
 
