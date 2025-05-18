7cabba1120a09c923db07245066962a629880a21
Leopold Blum <leopold.blum@gmx.de>
2025-05-18 03:55:15 +0200

    checkbutton is not spam clickable anymore

--- react-frontend/src/TodoCustomCheckmark.jsx
+++ react-frontend/src/TodoCustomCheckmark.jsx
@@ -11,20 +11,20 @@ const TodoCustomCheckmark = ({ currentTodo, checked }) => {
 
     const mutationEditTodo = useMutationEditTodo();
 
-    const hasTransitionStartedRef = useRef(true)
+    const hasTransitionStartedRef = useRef(false)
 
     useEffect(() => {
         setIsChecked(checked);
     }, [checked])
 
     const handleClick = () => {
-        hasTransitionStartedRef.current = false
+        hasTransitionStartedRef.current = true
         setIsChecked(prev => !prev);
     };
 
     const handleTransitionEnd = (e) => {
 
-        if ((e.propertyName !== "opacity" && e.propertyName !== "transform") || hasTransitionStartedRef.current) return;
+        if ((e.propertyName !== "opacity" && e.propertyName !== "transform") || !hasTransitionStartedRef.current) return;
 
         mutationEditTodo.mutate({
             id: currentTodo.id,
@@ -34,7 +34,7 @@ const TodoCustomCheckmark = ({ currentTodo, checked }) => {
             userid: todoFuncAndData.userIDref.current
         });
 
-        hasTransitionStartedRef.current = true
+        hasTransitionStartedRef.current = false
 
     };
 
@@ -42,12 +42,12 @@ const TodoCustomCheckmark = ({ currentTodo, checked }) => {
         <div className="flex justify-center items-center min-w-15 ">
 
 
-            <div className="flex justify-center items-center w-9 h-9 relative font-medium cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out group"
+            <div className={`flex justify-center items-center w-9 h-9 relative font-medium cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out group ${hasTransitionStartedRef.current ? "pointer-events-none" : ""}`}
                 onClick={handleClick}
                 onTransitionEnd={handleTransitionEnd}
             >
 
-                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out group-hover:text-text-hover-lm  ${!isChecked ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-75 rotate-45"}`}>
+                <div className={`absolute inset-0 object-cover transition-all duration-500 ease-in-out group-hover:text-text-hover-lm  ${!isChecked ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-75 rotate-45"} `}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2" /></svg>
                 </div>
 
